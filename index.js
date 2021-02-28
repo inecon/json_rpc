fcdc91abf2a37978d03a9c4f887523979e019515
Ievhen Perekhodchenko <i.perekhodchenko@favbet.com>
2021-02-28 19:54:17 +0200

    minor fixes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function start() {
             useUnifiedTopology: true
         })
         app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`)
+            logger.info(`Server is running on port ${PORT}`)
         })
     } catch (e) {
         logger.error(e)
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,15 +2,15 @@ const log4js = require("log4js");
 
 log4js.configure({
     appenders: {
-        out: { type: 'stdout' },
-        app: { type: 'file', filename: './logs/app.log' },
-        car: { type: 'file', filename: './logs/car.log' },
-        user: { type: 'file', filename: './logs/user.log' },
+        out: {type: 'stdout'},
+        app: {type: 'file', filename: './logs/app.log', daysToKeep: 7},
+        car: {type: 'file', filename: './logs/car.log', daysToKeep: 7},
+        user: {type: 'file', filename: './logs/user.log', daysToKeep: 7},
     },
     categories: {
-        default: { appenders: [ 'out', 'app' ], level: 'error' },
-        car: { appenders: [ 'out', 'car' ], level: 'debug' },
-        user: { appenders: [ 'out', 'user' ], level: 'debug' },
+        default: {appenders: ['out', 'app'], level: 'debug'},
+        car: {appenders: ['out', 'car'], level: 'debug'},
+        user: {appenders: ['out', 'user'], level: 'debug'},
     }
 });
 
